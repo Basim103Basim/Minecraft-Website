23cd666682dad1468499eecd7b6f2dc4387cf9e1
unknown <basimmalii103@gmail.com>
2026-08-24 16:24:59 +0500

    new update

--- src/pages/lore.jsx
+++ src/pages/lore.jsx
@@ -14,6 +14,24 @@ function Lorepage(){
                     <p>From a humble camp, the foundations of a kingdom were laid.</p>
                 </div>
             </NavLink>
+            <NavLink to="/lore/the-fall-of-the-dragon">
+                <div className="lore-box">
+                    <h1>The Fall of the Dragon</h1>
+                    <p>The dragon fell, and the three realms opened before them.</p>
+                </div>
+            </NavLink>
+            <NavLink to="/lore/the-withers-creation">
+                <div className="lore-box">
+                    <h1>The Wither's Creation</h1>
+                    <p>A forbidden creation born from the remnants of the Nether.</p>
+                </div>
+            </NavLink>
+            <NavLink to="/lore/the-expansion">
+                <div className="lore-box">
+                    <h1>The Expansion</h1>
+                    <p>With the great evils fallen, the two turned their eyes toward the wider world.</p>
+                </div>
+            </NavLink>
         </main>
     )
 }
